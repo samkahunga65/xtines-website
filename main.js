1b520e322e96bc824e135c7781b66de4ea2a0094
Intern ICT <ict.intern@cohesion.or.ke>
2022-12-01 13:09:46 +0300

    fixed some more styling and added a new carousel

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-new Glide('.glide', {
-    type: 'carousel',
-    perView: 1,
-    focusAt: 'center',
-    autoplay: 2700
-}).mount()
-
 document.getElementById("about-btn").addEventListener("click", e => {
     document.getElementById("landing").scrollIntoView({
         behavior: "smooth"
@@ -25,3 +18,31 @@ document.getElementById("contacts-btn").addEventListener("click", e => {
         behavior: "smooth"
     });
 });
+//setup carousel
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const slideWidth = slide.clientWidth;
+const backCarouselButton = document.getElementById("car-back");
+const forwardCarouselButton = document.getElementById("car-forward");
+
+let carImageDisplayed = -1;
+
+backCarouselButton.addEventListener("click", ()=>{
+	carImageDisplayed -= 1
+	if(carImageDisplayed <= -2 ) {
+		slidesContainer.scrollLeft += (2 * slideWidth);
+		carImageDisplayed = 1
+	} else {
+		slidesContainer.scrollLeft -= slideWidth;
+	}
+})
+forwardCarouselButton.addEventListener("click", ()=>{
+	carImageDisplayed += 1
+	if(carImageDisplayed >= 2 ) {
+		slidesContainer.scrollLeft -= (2 * slideWidth);
+		carImageDisplayed = -1
+	} else {
+		slidesContainer.scrollLeft += slideWidth;
+	}
+})
